237ad22199639c9456434c8ef4f233b11b42a601
luftkluft <luftkluft@gmail.com>
2018-10-31 16:09:55 +0300

    feature/test_gems: test rspec

---
+++ test_gems/spec/fast_spec.rb
@@ -0,0 +1,14 @@
+require '../test_gems/fast.rb'
+require 'rspec'
+
+describe "Fast" do
+  it "do something" do
+    print "it is rspec test"
+  end
+  it "do 2" do
+    print "it is rspec test_2"
+  end
+  it "do 3" do
+    print "it is rspec test_3"
+  end
+end
