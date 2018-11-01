168b940aa24050cb55fdf87bca3dd1cd7dd96da7
luftkluft <luftkluft@gmail.com>
2018-11-01 23:41:55 +0300

    feature/book: add library.load method

--- library/library.rb
+++ library/library.rb
@@ -7,4 +7,12 @@ class Library
   def initialize(books = [])
     @books = books
   end
+
+  def load(file_path = './db.yaml')
+    parsed = YAML.safe_load(File.open(file_path))
+    @books = parsed[:books]
+    puts 'Data loaded successfully.'
+  rescue ArgumentError => e
+    puts "Could not parse YAML: #{e.message}"
+  end
 end
