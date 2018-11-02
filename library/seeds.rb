70d13b433083e7ec3440d2c41e02d247812cba13
luftkluft <luftkluft@gmail.com>
2018-11-02 11:41:54 +0300

    feature/seeds: add object arrays and revision hesh

--- library/seeds.rb
+++ library/seeds.rb
@@ -1,7 +1,18 @@
 def seeds
+  readers = []
+
+  authors = []
+
   books = [
     Book.new('Book_1', 'Author_1')
   ]
 
-  { books: books }
+  orders = []
+
+  {
+    readers: readers,
+    authors: authors,
+    books: books,
+    orders: orders
+  }
 end
