bdfa1bd5fb855f14ba1f0a9f8f850796cca46978
luftkluft <luftkluft@gmail.com>
2018-11-02 15:14:32 +0300

    feature/top_reader: add library.all method

--- library/library.rb
+++ library/library.rb
@@ -14,6 +14,9 @@ class Library
   def load(file_path = 'library/db.yaml')
     parsed = YAML.load(File.open(file_path))
     @books = parsed[:books]
+    # @authors = parsed[:authors]
+    # @readers = parsed[:readers]
+    # @orders = parsed[:orders]
     puts 'Data loaded successfully.'
   rescue ArgumentError => e
     puts "Could not parse YAML: #{e.message}"
@@ -24,5 +27,12 @@ class Library
     puts 'Data saved successfully.'
   rescue ArgumentError => e
     puts "Could not save YAML: #{e.message}"
+  end
+
+  def all
+    puts "Books: #{@books}"
+    puts "Authors: #{@authors}"
+    puts "Readers: #{@readers}"
+    puts "Orders: #{@orders}"
   end
 end
