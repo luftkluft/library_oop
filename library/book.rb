5c6ddf0d348274cf3dfe7eeb5a9ecfef424f6fd4
luftkluft <luftkluft@gmail.com>
2018-11-12 00:20:28 +0300

    feature/pr_2: remastered 'book.rb'

--- library/book.rb
+++ library/book.rb
@@ -1,8 +1,12 @@
 class Book
-  attr_accessor :title, :author
+  attr_reader :title, :author
 
-  def initialize(title, author)
-    @title = title
-    @author = author
+  def initialize(args_hash)
+    @title = title[:title]
+    @author = author[:author]
+  end
+
+  def to_s
+    "Author: #{@author.name}.\nTitle: #{@title}."
   end
 end
