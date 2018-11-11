ec19c70ac15aa7f267dc857d0e97992cf3bb2f38
luftkluft <luftkluft@gmail.com>
2018-11-12 00:11:35 +0300

    feature/pr_2: remastered 'reader.rb'

--- library/reader.rb
+++ library/reader.rb
@@ -1,11 +1,15 @@
 class Reader
-  attr_accessor :name, :email, :city, :street, :house
+  attr_reader :name, :email, :city, :street, :house
 
-  def initialize(name, email, city, street, house)
-    @name = name
-    @email = email
-    @city = city
-    @street = street
-    @house = house
+  def initialize(args_hash)
+    @name = args_hash[:name]
+    @email = args_hash[:email]
+    @city = args_hash[:city]
+    @street = args_hash[:street]
+    @house = args_hash[:house]
+  end
+
+  def to_s
+    "Name: #{@name}\nE-mail: #{email}"
   end
 end
