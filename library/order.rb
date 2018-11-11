4718dab5916dc2d49252734fa71b523a5d292595
luftkluft <luftkluft@gmail.com>
2018-11-12 00:56:43 +0300

    feature/pr_2: remastered 'order.rb'

--- library/order.rb
+++ library/order.rb
@@ -1,9 +1,13 @@
 class Order
-  attr_accessor :book, :reader, :date
+  attr_reader :book, :reader, :date
 
-  def initialize(book, reader, date)
+  def initialize(book, reader, date = Date.today)
     @book = book
     @reader = reader
     @date = date
+  end
+
+  def to_s
+    "Book: '#{@book.title}' by #{@book.author.name}.\nReader: #{@reader.name} (#{@reader.email})\nOrder date: #{@date}."
   end
 end
