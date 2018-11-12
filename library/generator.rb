eaab7c483030061f3076e0eaa496b7df42f5530a
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-12 16:58:43 +0300

    Create generator.rb
    Create default data.

---
+++ library/generator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Generator
+  include FFakerData
+  def generate
+    return if File.exist?(INDEX_PATH)
+
+    begin
+      File.open(INDEX_PATH, 'w') { |f| f.write(generate_data.to_yaml) }
+      puts 'Default library created successfully.'
+    rescue StandardError => e
+      puts "Could not save YAML: #{e.message}"
+    end
+  end
+end
