baeb39035449f0c1614ab098a171c26cdef5e515
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-12 16:28:50 +0300

    Update order.rb
    Update order.rb.

--- library/order.rb
+++ library/order.rb
@@ -1,9 +1,17 @@
-class Order
-  attr_accessor :book, :reader, :date
+# frozen_string_literal: true
 
-  def initialize(book, reader, date)
+class Order
+  attr_reader :book, :reader, :date
+
+  def initialize(book, reader, date = Date.today)
     @book = book
     @reader = reader
     @date = date
+  end
+
+  def to_s
+    "Book: '#{@book.title}' by #{@book.author}.\n
+    Reader: #{@reader.name} (#{@reader.email})\n
+    Order date: #{@date}."
   end
 end
