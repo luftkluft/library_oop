e3ea8bd8cbfcc5fc5ea90d55786e3f9fb944a5ab
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-12 16:19:11 +0300

    Update author.rb
    Update author.rb

--- library/author.rb
+++ library/author.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 class Author
-  attr_accessor :name, :biography
+  attr_reader :name, :biography
 
   def initialize(name, biography)
     @name = name
     @biography = biography
+  end
+
+  def to_s
+    "Author: #{@name}.\nBiography: #{@biography}."
   end
 end
