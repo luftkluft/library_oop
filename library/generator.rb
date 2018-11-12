5d2f459470d49df2f3bf7e3262bedd553deb844d
luftkluft <luftkluft@gmail.com>
2018-11-12 04:51:08 +0300

    feature/pr_2: make guard clause in generator.rb

--- library/generator.rb
+++ library/generator.rb
@@ -3,13 +3,12 @@
 class Generator
   include FFakerData
   def generate
-    unless File.exist?(INDEX_PATH)
-      begin
-        File.open(INDEX_PATH, 'w') { |f| f.write(generate_data.to_yaml) }
-        puts 'Default library created successfully.'
-      rescue StandardError => e
-        puts "Could not save YAML: #{e.message}"
-      end
+    return if File.exist?(INDEX_PATH)
+    begin
+      File.open(INDEX_PATH, 'w') { |f| f.write(generate_data.to_yaml) }
+      puts 'Default library created successfully.'
+    rescue StandardError => e
+      puts "Could not save YAML: #{e.message}"
     end
   end
 end
