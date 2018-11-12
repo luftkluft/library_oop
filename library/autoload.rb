8637d3bab06f5c95d028caae5e3217044bceecdc
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-12 16:43:07 +0300

    Create autoload.rb
    Require all.

---
+++ library/autoload.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+INDEX_PATH = 'library/db.yaml'
+HELP_PATH = 'library/help.txt'
+require 'yaml'
+require 'ffaker'
+require 'date'
+require_relative 'reader'
+require_relative 'author'
+require_relative 'book'
+require_relative 'order'
+require_relative 'ffaker_data'
+require_relative 'generator'
+require_relative 'library'
