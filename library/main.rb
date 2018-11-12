b274a4dfc1ae2aeecee331d90b523a00d7801e9b
luftkluft <luftkluft@gmail.com>
2018-11-12 03:21:00 +0300

    feature/pr_2: remastered main.rb

--- library/main.rb
+++ library/main.rb
@@ -1,23 +1,8 @@
-require_relative 'library'
-require_relative 'seeds'
+require_relative 'autoload'
 
+generator = Generator.new
+generator.generate
 library = Library.new
-
-unless File.exist?(INDEX_PATH)
-  print 'Data are sown...'
-  sleep(1)
-  print '.'
-  sleep(1)
-  print '.'
-  sleep(1)
-  puts '.'
-
-  library.save(seeds)
-end
-
-library.load
-
-sleep(1)
 
 puts 'Welcome to Library'
 puts 'Enter \'help\' to see all available options'
@@ -29,16 +14,6 @@ loop do
   case input
   when /\Ahelp\z/i
     library.help
-  when /\Aall\z/i
-    library.all
-  when /\Atop_reader\z/i
-    library.top_reader
-  when /\Atop_book\z/i
-    library.top_book
-  when /\Atop_set\z/i
-    library.top_set
-  when /\Aadd_order\z/i
-    library.add_order
   when /\Aexit\z/i
     break
   else puts 'Invalid command (enter \'help\' to see all options).'
