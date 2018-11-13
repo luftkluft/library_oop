cf7e3db4791845f8d64b7b941fd38db8ce9cd6da
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-13 12:48:16 +0300

    Update generator.rb
    Updated PATH constant.

--- library/generator.rb
+++ library/generator.rb
@@ -3,10 +3,10 @@
 class Generator
   include FFakerData
   def generate
-    return if File.exist?(INDEX_PATH)
+    return if File.exist?(MAIN_PATH)
 
     begin
-      File.open(INDEX_PATH, 'w') { |f| f.write(generate_data.to_yaml) }
+      File.open(MAIN_PATH, 'w') { |f| f.write(generate_data.to_yaml) }
       puts 'Default library created successfully.'
     rescue StandardError => e
       puts "Could not save YAML: #{e.message}"
