be92e17fa97e5a582a24a79fef5d2ad372e752d3
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-13 12:54:46 +0300

    Create errors.rb
    Added Errors module.

---
+++ errors.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Errors
+  class ClassError < StandardError
+    def initialize
+      super('Сlass mismatch!')
+    end
+  end
+
+  class StringError < StandardError
+    def initialize
+      super('It is empty string!')
+    end
+  end
+end
