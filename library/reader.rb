355cf169cde355003e242614b3dadc05bb9c61b9
luftkluft <luftkluft@gmail.com>
2018-11-14 13:59:13 +0300

    feature/pr_3: rework reader.rb

--- library/reader.rb
+++ library/reader.rb
@@ -2,16 +2,22 @@
 
 class Reader
   attr_reader :name, :email, :city, :street, :house
+  include Validator
 
-  def initialize(args_hash)
-    @name = args_hash[:name]
-    @email = args_hash[:email]
-    @city = args_hash[:city]
-    @street = args_hash[:street]
-    @house = args_hash[:house]
+  def initialize(name:, email:, city:, street:, house:)
+    validate(name, email, city, street, house)
+    @name = name
+    @email = email
+    @city = city
+    @street = street
+    @house = house
   end
 
-  def to_s
-    "Name: #{@name}\nE-mail: #{email}"
+  def validate(name, email, city, street, house)
+    [name, email, city, street].each do |personal_data|
+      check_class(personal_data, String)
+      check_for_emptiness(personal_data)
+    end
+    check_class(house, Integer)
   end
 end
