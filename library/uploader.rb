41d0c7404567d62a981ed48028ec63ddd4f55b8e
luftkluft <luftkluft@gmail.com>
2018-11-14 13:13:45 +0300

    feature/pr_3: add uploader.rb with 'Uploader' module

---
+++ library/uploader.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Uploader
+  MAIN_PATH_ROUTE = 'library/'
+  MAIN_PATH_NAME = 'db'
+  MAIN_PATH_FORMAT = '.yml'
+  MAIN_PATH = MAIN_PATH_ROUTE + MAIN_PATH_NAME + MAIN_PATH_FORMAT
+
+  def save_to_db(book:, author:, reader:, order:)
+    library_db = [book, author, reader, order]
+    File.open(PATH, 'w') { |f| f.write library_db.to_yml }
+  end
+
+  private
+
+  def load_db
+    list = [Book, Reader, Author, Order, Date]
+    YAML.safe_load(File.read(PATH), list, [], [], true).to_a
+  end
+end
