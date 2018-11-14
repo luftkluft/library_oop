13526cb7d347782b2c4b750ba42420fa1f1f35e1
luftkluft <luftkluft@gmail.com>
2018-11-14 15:05:21 +0300

    feature/pr_3: rework library.rb

--- library/uploader.rb
+++ library/uploader.rb
@@ -8,13 +8,13 @@ module Uploader
 
   def save_to_db(book:, author:, reader:, order:)
     library_db = [book, author, reader, order]
-    File.open(PATH, 'w') { |f| f.write library_db.to_yml }
+    File.open(MAIN_PATH, 'w') { |f| f.write library_db.to_yml }
   end
 
   private
 
   def load_db
     list = [Book, Reader, Author, Order, Date]
-    YAML.safe_load(File.read(PATH), list, [], [], true).to_a
+    YAML.safe_load(File.read(MAIN_PATH), list, [], [], true).to_a
   end
 end
