8fab3e64055f327961cd89392c179ffaf0d8bdcf
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-14 20:00:15 +0300

    Update main.rb
    Reworked.

--- library/main.rb
+++ library/main.rb
@@ -1,25 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'autoload'
-
-generator = Generator.new
-generator.generate
-library = Library.new
-library.default_statistic
-
-puts 'Welcome to Library'
-puts 'Enter \'help\' to see all available options'
-
-loop do
-  puts '= Enter command ='
-  input = gets.chomp
-  case input
-  when /\Ahelp\z/i
-    library.help
-  when /\Aus\z/i
-    library.user_statistic
-  when /\Aexit\z/i
-    break
-  else puts 'Invalid command (enter \'help\' to see all options).'
-  end
-end
+require_relative 'db_data'
